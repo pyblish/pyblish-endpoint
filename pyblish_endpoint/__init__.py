7cb332008aa3d81aae09f222ab9dbb57d9c21b15
Marcus Ottosson <konstruktion@gmail.com>
2014-11-19 18:16:08 +0000

    Propagating logger

--- pyblish_endpoint/__init__.py
+++ pyblish_endpoint/__init__.py
@@ -2,10 +2,5 @@ import logging
 from .version import *
 
 # Initialise default logger
-formatter = logging.Formatter("%(levelname)s %(message)s")
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
 log = logging.getLogger("endpoint")
-log.addHandler(handler)
-log.setLevel(logging.WARNING)
+log.propagate = True
