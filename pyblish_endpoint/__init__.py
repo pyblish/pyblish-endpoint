d4552a38cd406614b3459f33b31daeff6c28faea
Marcus Ottosson <konstruktion@gmail.com>
2015-02-14 18:18:27 +0000

    Disabling default logger (can't figure out propagation to other libraries)

--- pyblish_endpoint/__init__.py
+++ pyblish_endpoint/__init__.py
@@ -1,19 +1,8 @@
 import os
 import sys
-import logging
 from .version import *
 
 # Register vendor packages
 package_dir = os.path.dirname(__file__)
 vendor_dir = os.path.join(package_dir, "vendor")
 sys.path.insert(0, vendor_dir)
-
-# Initialise default logger
-# __formatter = logging.Formatter("%(levelname)s - %(message)s")
-# __handler = logging.StreamHandler()
-# __handler.setFormatter(__formatter)
-# __log = logging.getLogger("endpoint")
-# __log.propagate = True
-# __log.handlers[:] = []
-# __log.addHandler(__handler)
-# __log.setLevel(logging.DEBUG)
