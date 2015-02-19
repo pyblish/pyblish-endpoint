3561904d9f40a6bca1d3cf65262a87eec2d114b7
Marcus Ottosson <konstruktion@gmail.com>
2015-02-19 17:48:15 +0000

    - Safeguarding instance and plugin serialising - Moving mocking into submodule

--- pyblish_endpoint/resource.py
+++ pyblish_endpoint/resource.py
@@ -18,7 +18,7 @@ import flask.ext.restful
 import flask.ext.restful.reqparse
 
 # Local library
-from service import current_service
+import service
 
 log = logging.getLogger("endpoint")
 
@@ -49,15 +49,40 @@ def format_error(error):
 
 
 def format_instance(instance):
+    """Serialise `instance`
+
+    For children to be visualised and modified,
+    they must provide an appropriate implementation
+    of __str__.
+
+    Data that isn't JSON compatible cannot be
+    visualised nor modified.
+
+    """
+
+    children = list()
+    for child in instance:
+        try:
+            child = str(child)
+        except:
+            child = "Invalid"
+        children.append(child)
+
+    data = dict()
+    for key, value in instance._data.iteritems():
+        try:
+            value = str(value)
+        except:
+            value = "Not supported"
+        data[key] = str(value)
+
     return {
         "name": instance.data("name"),
         "family": instance.data("family"),
         "objName": instance.name,
-        "category": getattr(instance, "category", None),
-        "nodes": [str(x) for x in instance],
-        "data": dict((str(k), str(v)) for k, v in instance._data.iteritems()),
+        "children": children,
+        "data": data,
         "publish": instance.data("publish"),
-        "doc": instance.data("doc")
     }
 
 
@@ -65,9 +90,9 @@ def format_plugin(plugin):
     formatted = {
         "name": plugin.__name__,
         "version": plugin.version,
+        "category": getattr(plugin, "category", None),
         "requires": plugin.requires,
         "order": plugin.order,
-        "active": False,
         "optional": plugin.optional,
         "doc": getattr(plugin, "doc", plugin.__doc__)
     }
@@ -114,8 +139,8 @@ class ApplicationApi(flask.ext.restful.Resource):
 
         data = {}
 
-        data.update(current_service().system())
-        data.update(current_service().versions())
+        data.update(service.current().system())
+        data.update(service.current().versions())
 
         return data, 200
 
@@ -174,7 +199,7 @@ class SessionApi(flask.ext.restful.Resource):
 
     def post(self):
         try:
-            status = current_service().init()
+            status = service.current().init()
         except Exception:
             _, _, exc_tb = sys.exc_info()
             tb = traceback.extract_tb(exc_tb)[-1]
@@ -195,7 +220,7 @@ class StateApi(flask.ext.restful.Resource):
     """
 
     def get(self):
-        return {"ok": True, "state": current_service().state}
+        return {"ok": True, "state": service.current().state}
 
     def post(self):
         parser = flask.ext.restful.reqparse.RequestParser()
@@ -214,7 +239,7 @@ class StateApi(flask.ext.restful.Resource):
             log.error(message)
             return {"ok": False, "message": message}, 500
 
-        current_service().state = state
+        service.current().state = state
         return {"ok": True, "state": state}, 200
 
 
@@ -226,7 +251,7 @@ class NextApi(flask.ext.restful.Resource):
     """
 
     def post(self):
-        result = current_service().next()
+        result = service.current().next()
 
         if result is not None:
             plugin = result["plugin"]
@@ -271,8 +296,8 @@ class PluginsListApi(flask.ext.restful.Resource):
 
         """
 
-        plugins = current_service().plugins
-        context = current_service().context
+        plugins = service.current().plugins
+        context = service.current().context
 
         families = set()
         for instance in context:
@@ -332,7 +357,7 @@ class InstancesListApi(flask.ext.restful.Resource):
 
         """
 
-        instances = list(current_service().context)
+        instances = list(service.current().context)
 
         response = []
         for instance in instances:
@@ -356,7 +381,7 @@ class InstancesApi(flask.ext.restful.Resource):
 
         """
 
-        instances = list(current_service().context)
+        instances = list(service.current().context)
 
         try:
             instance = find_instance(instance_id, instances)
@@ -383,7 +408,7 @@ class NodesListApi(flask.ext.restful.Resource):
 
         """
 
-        instances = list(current_service().context)
+        instances = list(service.current().context)
 
         try:
             instance = find_instance(instance_id, instances)
@@ -413,7 +438,7 @@ class DataListApi(flask.ext.restful.Resource):
 
         """
 
-        instances = list(current_service().context)
+        instances = list(service.current().context)
 
         try:
             instance = find_instance(instance_id, instances)
--- pyblish_endpoint/service.py
+++ pyblish_endpoint/service.py
@@ -157,135 +157,6 @@ class EndpointService(object):
             root_logger.removeHandler(handler)
 
 
-class MockService(EndpointService):
-    """Service for testing
-
-    Attributes:
-        SLEEP_DURATION: Fake processing delay, in milliseconds
-        NUM_INSTANCES: Fake amount of available instances (max: 2)
-        PERFORMANCE: Enum of fake performance. Available values are
-            SLOW, MODERATE, FAST, NATIVE
-
-    """
-
-    SLEEP_DURATION = 0
-    NUM_INSTANCES = 3
-
-    SLOW = 1 << 0
-    MODERATE = 1 << 1
-    FAST = 1 << 2
-    NATIVE = 1 << 3
-    PERFORMANCE = NATIVE
-
-    def init(self):
-        self.plugins = []
-        for plugin, superclass in (
-                ["ExtractAsMa", pyblish.api.Extractor],
-                ["ConformAsset", pyblish.api.Conformer]):
-            obj = type(plugin, (superclass,), {})
-
-            obj.families = ["napoleon.animation.cache"]
-            if plugin == "ConformAsset":
-                obj.families = ["napoleon.asset.rig"]
-
-            obj.hosts = ["python", "maya"]
-            self.plugins.append(obj)
-
-        fake_instances = ["Peter01", "Richard05", "Steven11"]
-        context = pyblish.api.Context()
-        for name in fake_instances[:self.NUM_INSTANCES]:
-            instance = context.create_instance(name=name)
-
-            instance._data = {
-                "identifier": "napoleon.instance",
-                "minWidth": 800,
-                "assetSource": "/server/assets/Peter",
-                "destination": "/server/published/assets",
-            }
-
-            instance.set_data("publish", True)
-
-            if name == "Peter01":
-                instance.set_data("publish", False)
-                instance.set_data("family", "napoleon.asset.rig")
-            else:
-                instance.set_data("family", "napoleon.animation.cache")
-
-            for node in ["node1", "node2", "node3"]:
-                instance.append(node)
-
-        self.plugins.append(ValidateFailureMock)
-        self.plugins.append(ValidateNamespace)
-        self.plugins = self.sort_plugins(self.plugins)
-
-        self.context = context
-        self.processor = None
-
-    def next(self):
-        result = super(MockService, self).next()
-        self.sleep()
-        return result
-
-    def sleep(self):
-        if self.SLEEP_DURATION:
-            log.info("Pretending it takes %s seconds "
-                     "to complete.." % self.SLEEP_DURATION)
-
-            performance = self.SLEEP_DURATION
-            if self.PERFORMANCE & self.SLOW:
-                performance += 2
-
-            if self.PERFORMANCE & self.MODERATE:
-                performance += 1
-
-            if self.PERFORMANCE & self.FAST:
-                performance += 0.1
-
-            if self.PERFORMANCE & self.NATIVE:
-                performance = 0
-
-            increment_sleep = performance / 3.0
-
-            time.sleep(increment_sleep)
-            log.info("Running first pass..")
-
-            time.sleep(increment_sleep)
-            log.info("Almost done..")
-
-            time.sleep(increment_sleep)
-            log.info("Completed successfully!")
-
-
-#
-# Mock classes
-#
-
-@pyblish.api.log
-class ValidateNamespace(pyblish.api.Validator):
-    families = ["napoleon.animation.cache"]
-    hosts = ["*"]
-    version = (0, 0, 1)
-
-    def process_instance(self, instance):
-        self.log.info("Validating namespace..")
-        self.log.info("Completed validating namespace!")
-
-
-@pyblish.api.log
-class ValidateFailureMock(pyblish.api.Validator):
-    families = ["*"]
-    hosts = ["*"]
-    version = (0, 0, 1)
-    optional = True
-
-    def process_instance(self, instance):
-        raise ValueError("Instance failed")
-
-#
-# End mock classes
-#
-
-
 class MessageHandler(logging.Handler):
     def __init__(self, records, *args, **kwargs):
         # Not using super(), for compatibility with Python 2.6
@@ -301,7 +172,8 @@ class MessageHandler(logging.Handler):
         self.records.append(record)
 
 
-def current_service():
+def current():
+    """Return currently active service"""
     return EndpointService._current
 
 
