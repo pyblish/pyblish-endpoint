4922630713b501bbb2e26a61948659e731baa972
Marcus Ottosson <konstruktion@gmail.com>
2015-04-17 15:48:28 +0100

    Updating mock to properly provide timing information

--- pyblish_endpoint/mocking.py
+++ pyblish_endpoint/mocking.py
@@ -1,5 +1,6 @@
 import time
 import logging
+import random
 
 import tests.lib
 
@@ -12,13 +13,23 @@ class MockService(tests.lib.TestService):
 
     SLEEP_DURATION = 0
 
-    def process(self, *args, **kwargs):
-        self.sleep()
-        return super(MockService, self).process(*args, **kwargs)
+    @classmethod
+    def sleep(cls):
+        duration = random.random() * cls.SLEEP_DURATION
+        if duration:
+            log.info("Pretending it takes %s ms "
+                     "to complete.." % duration * 1000)
 
-    def sleep(self):
-        if self.SLEEP_DURATION:
-            log.info("Pretending it takes %s seconds "
-                     "to complete.." % self.SLEEP_DURATION)
+            time.sleep(duration)
 
-            time.sleep(self.SLEEP_DURATION)
+
+def sleeper(func):
+    def wrapper(*args, **kwargs):
+        MockService.sleep()
+        return func(*args, **kwargs)
+    return wrapper
+
+
+for plugin in tests.lib.PLUGINS:
+    plugin.process_instance = sleeper(plugin.process_instance)
+    plugin.process_context = sleeper(plugin.process_context)
